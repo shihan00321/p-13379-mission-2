3d3dbf742fa35dc58137776a70b9df9a68da9fd8
shihan00321 <shihan001@naver.com>
2025-06-03 15:57:48 +0900

    TDD Test1~17 성공 케이스 구현

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -1,23 +1,48 @@
 package com.back;
 
 public class Calc {
+    /**
+     * 스택 자료구조 사용 X, 재귀를 사용
+     * 1. 연산식을 먼저 전부 나눈다.  [20, +, 10, +, 5, *, 2]
+     * 2. 앞에서 부터 하나씩 확인 - 인덱스 < 위 배열 길이
+     * 3. 곱셈이 먼저 계산되어야하므로 덧,뺄셈 -> 곱셈을 호출해야 곱셈이 먼저 처리되고 돌아온다.
+     */
+    static String[] split;
+    static int curIndex;
+
     public static int run(String input) {
-        String[] split = input.split(" ");
-        int sum = Integer.parseInt(split[0]);
-        int curIndex = 1;
+        split = input.split(" ");
+        curIndex = 0;
+        return addOrSub();
+    }
+
+    public static int addOrSub() {
+        int sum = multiple();
         while (curIndex < split.length) {
-            sum = operation(sum, Integer.parseInt(split[curIndex + 1]), split[curIndex]);
-            curIndex += 2;
+            String operator = split[curIndex];
+            if (operator.equals("+")) {
+                curIndex++;
+                sum += multiple();
+            }
+            if (operator.equals("-")) {
+                curIndex++;
+                sum -= multiple();
+            }
         }
         return sum;
     }
 
-    public static int operation(int sum, int num, String operator) {
-        return switch (operator) {
-            case "+" -> sum + num;
-            case "-" -> sum - num;
-            case "*" -> sum * num;
-            default -> 0;
-        };
+    public static int multiple() {
+        int curNum = Integer.parseInt(split[curIndex]);
+        curIndex++;
+        while (curIndex < split.length) {
+            String operator = split[curIndex];
+            if (!operator.equals("*")) {
+                break;
+            }
+            curNum *= Integer.parseInt(split[++curIndex]);
+            curIndex++;
+        }
+        return curNum;
     }
 }
